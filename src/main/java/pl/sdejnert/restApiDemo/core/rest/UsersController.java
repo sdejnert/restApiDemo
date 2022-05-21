d303ba6627d6356672236c20487ccdd562f64fc4
Sebastian Dejnert <sebastian.dejnert@almar.pl>
2022-05-21 15:06:39 +0200

    Dodano walidację danych dla loginu

--- src/main/java/pl/sdejnert/restApiDemo/core/rest/UsersController.java
+++ src/main/java/pl/sdejnert/restApiDemo/core/rest/UsersController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sdejnert.restApiDemo.core.dto.ResponseMessage;
 import pl.sdejnert.restApiDemo.core.rest.external.GitUsersService;
+import pl.sdejnert.restApiDemo.core.rest.validation.Validator;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,13 +19,17 @@ public class UsersController {
 
     private final UsersService usersService;
     private final GitUsersService gitUsersService;
+    private final Validator validator;
 
     @GetMapping("/user/{login}")
     public ResponseEntity<?> findByLogin(@PathVariable final String login) {
         log.info(String.format("Zapytanie dla loginu: %s", login));
-        return gitUsersService.findGitUserByLogin(login).fold(
-                error -> ResponseEntity.badRequest().body(new ResponseMessage(error)),
-                gitUserResponseDto -> ResponseEntity.ok(usersService.saveCallAndReturnUserResponseDto(gitUserResponseDto))
-        );
+        List<String> errors = validator.validateLogin(login);
+        if (errors.isEmpty())
+            return gitUsersService.findGitUserByLogin(login).fold(
+                    error -> ResponseEntity.badRequest().body(new ResponseMessage(error)),
+                    gitUserResponseDto -> ResponseEntity.ok(usersService.saveCallAndReturnUserResponseDto(gitUserResponseDto))
+            );
+        return ResponseEntity.badRequest().body(new ResponseMessage(errors.toString()));
     }
 }
